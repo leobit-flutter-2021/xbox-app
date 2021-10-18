d6426efcc1ec96ede54c1325e30894eb36cc9cfd
aekrops <svyatoslavtrostynski24@gmail.com>
2021-10-18 04:00:29 +0300

    added social screen

---
+++ lib/social_view/social.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:lab_2_try/navigation/bottom_navigation.dart';
+
+import 'element/friends_list.dart';
+import 'element/social_header.dart';
+
+class Social extends StatelessWidget {
+  const Social({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double orientationHeight =
+        (MediaQuery.of(context).orientation == Orientation.landscape)
+            ? 0.35
+            : 0.2;
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const SocialHeader(),
+          toolbarHeight:
+              MediaQuery.of(context).size.height * orientationHeight),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          children: const [OnlineFriends(), OfflineFriends()],
+        ),
+      )),
+      bottomNavigationBar: const BottomAppBarNavigation(),
+    );
+  }
+}
